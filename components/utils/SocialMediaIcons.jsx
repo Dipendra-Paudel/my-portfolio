da544b51d14e9e828e7ec446e7ff1ce5c9e5a174
Dipendra-Paudel <dienbro006@gmail.com>
2022-08-25 22:59:59 +0545

    social sharing widget icon added

--- components/utils/SocialMediaIcons.jsx
+++ components/utils/SocialMediaIcons.jsx
@@ -1,37 +1,25 @@
 import React from "react";
 import {
+  LinkedinShareButton,
   LinkedinIcon,
+  TwitterShareButton,
   TwitterIcon,
-  TelegramIcon,
-  EmailIcon,
-  FacebookIcon,
 } from "react-share";
 
 const socialMediaIcons = [
   {
-    url: "https://www.linkedin.com/in/dipendra-paudel-750267249/",
-    icon: <LinkedinIcon size={32} round={true} />,
-    label: "Linkedin",
+    component: (
+      <LinkedinShareButton url="https://www.linkedin.com/in/dipendra-paudel-750267249/">
+        <LinkedinIcon size={32} round />
+      </LinkedinShareButton>
+    ),
   },
   {
-    url: "https://twitter.com/dip_endrapaudel",
-    icon: <TwitterIcon size={32} round={true} />,
-    label: "Twitter",
-  },
-  {
-    url: "https://t.me/dipen_dra",
-    icon: <TelegramIcon size={32} round={true} />,
-    label: "Twitter",
-  },
-  {
-    url: "https://twitter.com/dip_endrapaudel",
-    icon: <EmailIcon size={32} round={true} />,
-    label: "Twitter",
-  },
-  {
-    url: "https://t.me/dipen_dra",
-    icon: <FacebookIcon size={32} round={true} />,
-    label: "Twitter",
+    component: (
+      <TwitterShareButton url="https://twitter.com/dip_endrapaudel">
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+    ),
   },
 ];
 
@@ -39,19 +27,8 @@ const SocialMediaIcons = ({ classes = "flex space-x-2", footer }) => {
   return (
     <div className={`${classes} ${footer ? "" : "text-gray-600"}`}>
       {socialMediaIcons.map((socialMedia, index) => {
-        const { url, icon, label } = socialMedia;
-        return (
-          <a
-            href={url}
-            key={index}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={label}
-            className={footer ? "text-gray-100" : "hover:text-primaryDark"}
-          >
-            {icon}
-          </a>
-        );
+        const { component } = socialMedia;
+        return <div key={index}>{component}</div>;
       })}
     </div>
   );
